b503f8c45f8a306aea6629f2839d623b5781479f
atclarkson <atclarkson@gmail.com>
2018-05-16 10:49:15 -0400

    Update to pass new grader in ND program.

--- designs.js
+++ designs.js
@@ -1,12 +1,5 @@
 // Select color input
 var colorPick = document.getElementById("colorPicker");
-var color = "#000000";
-colorPick.addEventListener("input", function() {
-  color = this.value;
-  console.log(color);
-});
-
-
 
 
 // Select size input
@@ -14,24 +7,23 @@ var columns;
 var rows;
 var submitGrid = document.getElementById("sizePicker").addEventListener("submit", function(evt) {
   evt.preventDefault();
-  rows = $('input#input_height').val();
-  columns = $('input#input_width').val();
-  $('#pixel_canvas').children('*').remove();
-  $( "#pixel_canvas").html(makeGrid());
+  rows = $('input#inputHeight').val();
+  columns = $('input#inputWidth').val();
+  $('#pixelCanvas').children('*').remove();
+  $( "#pixelCanvas").html(makeGrid());
 });
-
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-  for (var r = 0; r < rows; r++){
-    $( "#pixel_canvas" ).append( "<tr id='row" + r + "'></tr>" );
-    for (var c = 0; c < columns; c++) {
-      $( "#row" + r ).append( "<td id='cell'></td>" );
+  for (let r = 0; r < rows; r++){
+    $( "#pixelCanvas" ).append( "<tr id='row" + r + "'></tr>" );
+    for (let c = 0; c < columns; c++) {
+      $( "#row" + r ).append( "<td style='background-color: rgb(255, 255, 255);'></td>" );
     }
   }
 }
 
-$('#pixel_canvas').on('click', 'td', function(){
-      $(this).css('background', color);
+$('#pixelCanvas').on('click', 'td', function(){
+      $(this).css('background-color', colorPick.value);
     });
